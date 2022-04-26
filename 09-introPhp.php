41a81c164050773a9bad4989caaadf8c34fe9734
Marcelo Bauer Sabaraense <bauersaba@gmail.com>
2022-04-26 20:24:18 -0300

    Intrud PHP 1

---
+++ 09-introPhp.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt = "Sistemas";
+$num = 5;
+echo "My first PHP script! - Marcelo Bauer $txt $num <br>";
+$x = 5;
+$y = 5;
+echo $x + $y;
+?>
+
+</body>
+</html>
