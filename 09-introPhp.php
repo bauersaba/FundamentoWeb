294804c98f93395c76d3d9263d03ad0146bd82c4
Marcelo Bauer Sabaraense <bauersaba@gmail.com>
2022-06-09 06:00:34 -0300

    Update 09-introPhp.php

--- 09-introPhp.php
+++ 09-introPhp.php
@@ -4,14 +4,14 @@
 
 <?php
 
-$txt = "Sistemas";
+$txt = "Sistemas"; // Variavel sempre com cifrão na frente. Ele assume o tipo conforme o dado.
 $num = 5;
-echo "My first PHP script! - Marcelo Bauer $txt $num <br>";
+echo "My first PHP script! - Marcelo Bauer $txt $num <br>"; // para printar na tela. Semelhante ao print
 $x = 5;
 $y = 5;
 echo $x + $y ;
 echo "<br>";
-var_dump($txt)
+var_dump($txt) // Verificar tipo da variavel.
 
 
 
